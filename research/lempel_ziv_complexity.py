64714d2bac68b38d1dc3b3c55e8689b8f3b1a756
Ryan Thorpe <ryvthorpe@gmail.com>
2022-07-27 17:41:00 -0400

    lempel ziv for multiple channels

--- research/lempel_ziv_complexity.py
+++ research/lempel_ziv_complexity.py
@@ -70,7 +70,7 @@ def lempel_ziv_decomposition(sequence):
     return list(sub_strings)
 
 
-def lempel_ziv_complexity(sequence):
+def lempel_ziv_complexity(sequence, sub_strings=None):
     r""" Manual implementation of the Lempel-Ziv complexity.
 
     It is defined as the number of different substrings encountered as the stream is viewed from begining to the end.
@@ -98,8 +98,16 @@ def lempel_ziv_complexity(sequence):
     - Note: see this notebook for more details, comparison, benchmarks and experiments: https://Nbviewer.Jupyter.org/github/Naereen/Lempel-Ziv_Complexity/Short_study_of_the_Lempel-Ziv_complexity.ipynb
     - Note: there is also a Cython-powered version, for speedup, see :download:`lempel_ziv_complexity_cython.pyx`.
     """
-    sub_strings = set()
-    n = len(sequence)
+    if sub_strings is None:
+        sub_strings = set()
+
+    if isinstance(sequence, list):
+        if len(sequence) > 1:
+            component_seq = sequence[:-1]
+            _, sub_strings = lempel_ziv_complexity(component_seq,
+                                                   sub_strings=sub_strings)
+
+        sequence = sequence[-1]
 
     ind = 0
     inc = 1
@@ -113,7 +121,62 @@ def lempel_ziv_complexity(sequence):
             sub_strings.add(sub_str)
             ind += inc
             inc = 1
-    return len(sub_strings)
+    return len(sub_strings), sub_strings
+
+
+def lempel_ziv_complexity_md(seq_components, sub_strings=None):
+    r""" Manual implementation of the Lempel-Ziv complexity.
+
+    It is defined as the number of different substrings encountered as the stream is viewed from begining to the end.
+    As an example:
+
+    >>> s = '1001111011000010'
+    >>> lempel_ziv_complexity(s)  # 1 / 0 / 01 / 11 / 10 / 110 / 00 / 010
+    8
+
+    Marking in the different substrings the sequence complexity :math:`\mathrm{Lempel-Ziv}(s) = 8`: :math:`s = 1 / 0 / 01 / 11 / 10 / 110 / 00 / 010`.
+
+    - See the page https://en.wikipedia.org/wiki/Lempel-Ziv_complexity for more details.
+
+
+    Other examples:
+
+    >>> lempel_ziv_complexity('1010101010101010')  # 1, 0, 10, 101, 01, 010, 1010
+    7
+    >>> lempel_ziv_complexity('1001111011000010000010')  # 1, 0, 01, 11, 10, 110, 00, 010, 000
+    9
+    >>> lempel_ziv_complexity('100111101100001000001010')  # 1, 0, 01, 11, 10, 110, 00, 010, 000, 0101
+    10
+
+    - Note: it is faster to give the sequence as a string of characters, like `'10001001'`, instead of a list or a numpy array.
+    - Note: see this notebook for more details, comparison, benchmarks and experiments: https://Nbviewer.Jupyter.org/github/Naereen/Lempel-Ziv_Complexity/Short_study_of_the_Lempel-Ziv_complexity.ipynb
+    - Note: there is also a Cython-powered version, for speedup, see :download:`lempel_ziv_complexity_cython.pyx`.
+    """
+    if sub_strings is None:
+        sub_strings = set()
+
+    if isinstance(seq_components, list):
+        sequence = seq_components[0]
+    else:
+        sequence = seq_components
+
+    ind = 0
+    inc = 1
+    while True:
+        if ind + inc > len(sequence):
+            break
+        sub_str = sequence[ind : ind + inc]
+        if sub_str in sub_strings:
+            inc += 1
+        else:
+            sub_strings.add(sub_str)
+            ind += inc
+            inc = 1
+
+    if len(seq_components) > 1 and isinstance(seq_components, list):
+        _, sub_strings = lempel_ziv_complexity(seq_components[1:],
+                                               sub_strings=sub_strings)
+    return len(sub_strings), sub_strings
 
 
 
@@ -121,6 +184,16 @@ def lempel_ziv_complexity(sequence):
 
 if __name__ == "__main__":
     # Code for debugging purposes.
-    from doctest import testmod
-    print("\nTesting automatically all the docstring written in each functions of this module :")
-    testmod(verbose=True)
+    #from doctest import testmod
+    #print("\nTesting automatically all the docstring written in each functions of this module :")
+    #testmod(verbose=True)
+
+    seq_components = ['10110100111001010010100000011',
+                      '00100111000110001011010000110',
+                      '11010011010000101100001011010']
+    complexity, _ = lempel_ziv_complexity_md(seq_components)
+    print(f'Lempel-Ziv complexity for {seq_components}: {complexity}')
+
+    agg_seq = seq_components[0] + seq_components[1] + seq_components[2]
+    complexity, _ = lempel_ziv_complexity_md(agg_seq)
+    print(f'Lempel-Ziv complexity for {agg_seq}: {complexity}')
